7b7077e9825a08b5bb04b55ed8f2c4124dbb8641
Athar Shakeel <82315023+athar05@users.noreply.github.com>
2023-10-16 00:59:06 +0530

    add project and client queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,29 @@
-const { projects, clients } = require("../sampleData.js");
+const { projects, clients } = require("./sampleData");
 const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
+
+// Project Type
+
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return client.find((client) => client.id === project.clientId);
+      },
+    },
+  }),
+});
 
 // Client Type
 
@@ -21,6 +40,25 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
